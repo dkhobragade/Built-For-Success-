3273079f02764c5a2b6b6a066eefed1db706edcc
dkhobragade <dikshantkhobragade12@gmail.com>
2025-06-30 23:49:12 +0530

    Add DSAContent constant and implement Page component to display a roadmap for mastering Data Structures & Algorithms

--- frontend/app/(with-sidebar)/dsa/page.tsx
+++ frontend/app/(with-sidebar)/dsa/page.tsx
@@ -1,8 +1,28 @@
-import { Box } from "@mui/material";
+import { DSAContent } from "@/lib/constants";
+import { Box, List, ListItem, Typography } from "@mui/material";
 
 export default function Page ()
 {
-    return <Box>
-        dj
+
+    const renderList = () =>
+    {
+        return <>
+            { DSAContent.map( ( text, idx ) => <ListItem key={ idx }>
+                { text }
+            </ListItem> ) }
+        </>
+    }
+    return <Box padding={ 2 }  >
+        <Typography fontSize={ 25 } fontStyle="italic" fontWeight={ 500 } >
+            A complete roadmap for mastering Data Structures & Algorithms (DSA) from scratch to advanced — structured, beginner-friendly, and absolutely free.
+        </Typography>
+        <Typography component="div" >
+            <List>
+                <ListItem>
+                    🔑 Key Highlights
+                </ListItem>
+                { renderList() }
+            </List>
+        </Typography>
     </Box>
 }
